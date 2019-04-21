c2718d8b756d13ea22aa419f41c2a45311728457
Romil Parhwal <romilparhwal007@gmail.com>
2019-04-21 22:13:04 +0000

    We're lazy
    Change-Id: Ie1bc23e56bb7ba0ee8184377eba1ff156ba8e623

--- envsetup.sh
+++ envsetup.sh
@@ -138,7 +138,7 @@ function check_product()
         return
     fi
     if (echo -n $1 | grep -q -e "^lineage_") ; then
-        LINEAGE_BUILD=$(echo -n $1 | sed -e 's/^lineage_//g')
+        LINEAGE_BUILD=$(echo -n $1 | sed -e 's/^lazy_//g')
         export BUILD_NUMBER=$( (date +%s%N ; echo $LINEAGE_BUILD; hostname) | openssl sha1 | sed -e 's/.*=//g; s/ //g' | cut -c1-10 )
     else
         LINEAGE_BUILD=
